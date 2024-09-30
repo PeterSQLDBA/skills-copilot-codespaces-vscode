99e0e64a89ea4d2bacee9443379e82e79d1ddcc1
PeterSQLDBA <monadressecourriel@bell.net>
2024-09-30 09:35:22 -0400

    2e commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  const member = {
+    name: "Cameron",
+    role: "Software Engineer",
+    skills: ["JavaScript", "TypeScript", "React", "Node.js", "GraphQL", "SQL", "Docker"],
+    funFact: "I love to play guitar and write music!"
+  };
+  return member;
+}
